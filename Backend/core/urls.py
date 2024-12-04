47c0c5bdcac49d073933a2c7a75760c5a944343b
Rai Umer <raifarooq7860786@gmail.com>
2024-12-05 03:57:45 +0500

    Friend request sending

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
      path('users/profile/', ProfileView.as_view(), name='user-profile'),
      path("post/",AddPost.as_view(),name="Add-post"),
      path("users/find/",FindProducerView.as_view(),name="Find-producer"),
+     path('friend-request/send/', SendFriendRequestView.as_view(), name='send-friend-request'),
+     path('friend-request/manage/<int:request_id>/', ManageFriendRequestView.as_view(), name='manage-friend-request'),
+     path('friend-requests/', ListFriendRequestsView.as_view(), name='list-friend-requests'),
 ]
